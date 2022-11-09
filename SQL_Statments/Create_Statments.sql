2c5e402204624c6c39470e1902a740a385622e8a
NathanRomphf <nathan.romphf@ucalgary.ca>
2022-11-09 14:36:32 -0700

    Some Tables

--- SQL_Statments/Create_Statments.sql
+++ SQL_Statments/Create_Statments.sql
@@ -7,7 +7,30 @@ CREATE TABLE User
     (Email      VARCHAR(15)     NOT NULL,
     FName       VARCHAR(15)     NOT NULL,
     LName       VARCHAR(15)     NOT NULL,
-    Passwork    VARCHAR(15)     NOT NULL,
+    Password_    VARCHAR(15)     NOT NULL,
     UserType    VARCHAR(15)     NOT NULL,
     AdminEmail  VARCHAR(15)     NOT NULL,
+    PRIMARY KEY (Email)
+    )
+
+CREATE TABLE Admin_
+    (Email      VARCHAR(15)     NOT NULL,
+    Password_   VARCHAR(15)     NOT NULL,
+    Name_       VARCHAR(15)     NOT NULL,
+    Role_       VARCHAR(15)     NOT NULL,
+    PRIMARY KEY(Email)
+    )
+
+CREATE TABLE Viewer
+    (Email      VARCHAR(15)     NOT NULL,
+    SEmail      VARCHAR(15)     NOT NULL,
+    SchedID     INT,            NOT NULL,
+    PRIMARY KEY(Email)
+    )
+
+CREATE TABLE Student
+    (Email      VARCHAR(15)     NOT NULL,
+    Major       VARCHAR(15)     NOT NULL,
+    Year_        INT             NOT NULL,
+    PRIMARY KEY(Email)
     )
