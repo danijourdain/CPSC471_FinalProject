e5cdb44e4072aa6b4766329f6b518ab0e6abd757
danijourdain <danijourdain@icloud.com>
2022-11-09 10:43:16 -0700

    Adding first progress on SQL statments

---
+++ SQL_Statments/Create_Statments.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS '471_Final_Project';
+CREATE DATABASE '471_Final_Project';
+--remove the database if it already exists, and create a new database
+
+
+CREATE TABLE User
+    (Email      VARCHAR(15)     NOT NULL,
+    FName       VARCHAR(15)     NOT NULL,
+    LName       VARCHAR(15)     NOT NULL,
+    Passwork    VARCHAR(15)     NOT NULL,
+    
+    )
