097c15adc199cb066532e6c436776ce381907481
NathanRomphf <nathan.romphf@ucalgary.ca>
2022-11-11 11:51:11 -0700

    Fixed Create Statements

--- SQL_Statments/Create_Statments.sql
+++ SQL_Statments/Create_Statments.sql
@@ -13,113 +13,28 @@ CREATE TABLE Admin_
     PRIMARY KEY(Email)
     );
 
-    CREATE TABLE User
+CREATE TABLE User_
     (Email      VARCHAR(32)     NOT NULL,
     FName       VARCHAR(15)     NOT NULL,
     LName       VARCHAR(15)     NOT NULL,
     Password_    VARCHAR(15)     NOT NULL,
     UserType    VARCHAR(15)     NOT NULL,
     AdminEmail  VARCHAR(15)     NOT NULL,
-    PRIMARY KEY (Email)
-    FOREGIN KEY(AdminEmail) REFERENCES Admin_(Email)
+    PRIMARY KEY (Email),
+    FOREIGN KEY(AdminEmail) REFERENCES Admin_(Email)
     ON DELETE CASCADE       ON UPDATE CASCADE
     );
-
-CREATE TABLE Viewer
-    (Email      VARCHAR(32)     NOT NULL,
-    SEmail      VARCHAR(15)     NOT NULL,
-    SchedID     INT,            NOT NULL,
-    PRIMARY KEY(Email, SchedID),
-    FOREGIN KEY(Email) REFERENCES User(Email),
-    FOREGIN KEY(SchedID) REFERENCES Schedule(SchedID),
-    FOREGIN KEY(SEmail) REFERENCES Student(Email)
-    );
-
+    
 CREATE TABLE Student
     (Email      VARCHAR(32)     NOT NULL,
     Major       VARCHAR(15)     NOT NULL,
     Year_        INT             NOT NULL,
     PRIMARY KEY(Email),
-    FOREGIN KEY(Email) REFERENCES User(Email),
+    FOREIGN KEY(Email) REFERENCES User_(Email)
     ON DELETE CASCADE       ON UPDATE CASCADE
     );
-
-
-CREATE TABLE Course
-    (CName      VARCHAR(15)     NOT NULL,
-    CNumber     INT             NOT NULL,
-    Descrip     VARCHAR(15),
-    PRIMARY KEY(CName, CNumber),
-    ON DELETE CASCADE       ON UPDATE CASCADE
-    );
-
-CREATE TABLE Student_Takes
-    (StuEmail   VARCHAR(15)     NOT NULL,
-    CName       VARCHAR(15)     NOT NULL,
-    CNumber     INT             NOT NULL,
-    Grade       INT,
-    PRIMARY KEY(StuEmail, CName, CNumber),
-    FOREIGN KEY(StuEmail) REFERENCES Student(Email),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber)
-    );
-
-CREATE TABLE Student_Course
-    (SEmail     VARCHAR(15)     NOT NULL,
-    CName       VARCHAR(15)     NOT NULL,
-    CNumber     INT             NOT NULL,
-    PRIMARY KEY(SEmail, CName, CNumber),
-    FOREIGN KEY(SEmail) REFERENCES Student(Email),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber)
-    );
-
-CREATE TABLE Exam_Topic
-    (ExamName   VARCHAR(15)     NOT NULL,
-    Topic       VARCHAR(15)     NOT NULL,
-    PRIMARY KEY(ExamName, Topic),
-    FOREIGN KEY(ExamName) REFERENCES Exam(Name_)
-    );
-
-CREATE TABLE Assignment
-    (Name_      VARCHAR(15)     NOT NULL,
-    CNumber     VARCHAR(15)     NOT NULL,
-    CName       VARCHAR(15)     NOT NULL,
-    Weight_     INT             NOT NULL,
-    Due_Date    DATE            NOT NULL,
-    Descrip     VARCHAR(15),
-    Contact     VARCHAR(15),
-    ListID      INT             NOT NULL,
-    PRIMARY KEY(Name_, CNumber, CName),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(ListID) REFERENCES To_Do_List(ListID),
-    ON DELETE CASCADE       ON UPDATE CASCADE
-    );
-
-CREATE TABLE Completes_Assignmetns
-    (SEmail     VARCHAR(15)     NOT NULL,
-    AName       VARCHAR(15)     NOT NULL,
-    CName       VARCHAR(15)     NOT NULL,
-    CNumber     VARCHAR(15)     NOT NULL,
-    PRIMARY KEY(SEmail, AName, CName, CNumber),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(AName) REFERENCES Assignment(Name_)
-    FOREIGN KEY(SEmail) REFERENCES Student(Email)
-    );
-
-CREATE TABLE Attends_Class
-(
-    CMeetingName    VARCHAR(25)     NOT NULL,
-    SEmail          VARCHAR(32)     NOT NULL,
-    PRIMARY KEY(CMeetingName, SEmail),
-    FOREIGN KEY(CMeetingName) REFERENCES Class_Meeting(MeetingName),
-    FOREIGN KEY(SEmail) REFERENCES Student(Email)
-    ON DELETE CASCADE       ON UPDATE CASCADE
-);
-
-CREATE TABLE Schedule
+    
+CREATE TABLE Schedule_
 (
     ID              INT             NOT NULL,
     StartDate       DATE            NOT NULL,
@@ -129,71 +44,52 @@ CREATE TABLE Schedule
     StudentEmail    VARCHAR(32)     NOT NULL,
     PRIMARY KEY(ID),
     FOREIGN KEY(StudentEmail) REFERENCES Student(Email)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
-CREATE TABLE To_Do_List
-(
-    ListID          INT             NOT NULL,
-    SEmail          VARCHAR(32)     NOT NULL,
-    PRIMARY KEY(ListID),
+CREATE TABLE Viewer
+    (Email      VARCHAR(32)     NOT NULL,
+    SEmail      VARCHAR(15)     NOT NULL,
+    SchedID     INT             NOT NULL,
+    PRIMARY KEY(Email, SchedID),
+    FOREIGN KEY(Email) REFERENCES User_ (Email)
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(SchedID) REFERENCES Schedule_(ID)
+       ON DELETE CASCADE       ON UPDATE CASCADE,
     FOREIGN KEY(SEmail) REFERENCES Student(Email)
-);
+       ON DELETE CASCADE       ON UPDATE CASCADE
+    );
 
-CREATE TABLE Tasks
-(
-    ListID          INT             NOT NULL,
-    Task            VARCHAR(50)     NOT NULL,
-    PRIMARY KEY(ListID, Task),
-    FOREIGN KEY(ListID) REFERENCES To_Do_List(ListID)
-);
+CREATE TABLE Course
+    (CName      CHAR(4)     NOT NULL,
+    CNumber        INT             NOT NULL,
+    Descrip     VARCHAR(15)		DEFAULT NULL,
+    PRIMARY KEY(CName, CNumber)
+    );
+    
+CREATE TABLE Student_Takes
+    (StuEmail   VARCHAR(32)     NOT NULL,
+    CName       CHAR(4)     NOT NULL,
+    CNumber       INT             NOT NULL,
+    Grade       INT				DEFAULT NULL,
+    PRIMARY KEY(CName, CNumber),
+    FOREIGN KEY(StuEmail) REFERENCES Student(Email)
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+      ON DELETE CASCADE       ON UPDATE CASCADE
+    );
 
-CREATE TABLE Section 
-(
-    CName           CHAR(4)         NOT NULL,
-    CNumber         INT             NOT NULL,
-    LectureSection  CHAR(3)         NOT NULL,
-    Semester        VARCHAR(10)     NOT NULL,
-    PRIMARY KEY(CName, CNumber, LectureSection, Semester),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber)
-);
-
-CREATE TABLE Group_Meeting
-(
-    ID              INT             NOT NULL,
-    Name_           VARCHAR(15)     NOT NULL,
-    CName           CHAR(4)         NOT NULL,
-    CNumber         INT             NOT NULL,
-    PRIMARY KEY(ID),
-    FOREIGN KEY(CName) REFERENCES Course(CName),
-    FOREIGN KEY(CNumber) REFERENCES Course(CNumber)
-    ON DELETE CASCADE       ON UPDATE CASCADE
-);
-
-CREATE TABLE Group_Meeting_Date
-(
-    GroupID         INT             NOT NULL,
-    Date_           DATE            NOT NULL,
-    PRIMARY KEY(GroupID, Date_),
-    FOREIGN KEY(GroupID) REFERENCES Group_Meeting(ID)
-);
-
-CREATE TABLE Group_Meeting_Members
-(
-    GroupID         INT             NOT NULL,
-    Member          VARCHAR(50)     NOT NULL,
-    PRIMARY KEY(GroupID, Member),
-    FOREIGN KEY(GroupID) REFERENCES Group_Meeting(ID)
-);
-
-CREATE TABLE Attends_Group_Meeting
-(
-    MeetingID       INT             NOT NULL,
-    SEmail          VARCHAR(32)     NOT NULL,
-    PRIMARY KEY(MeetingID, SEmail),
-    FOREIGN KEY(MeetingID) REFERENCES Group_Meeting(ID),
+CREATE TABLE Student_Course
+    (SEmail     VARCHAR(15)     NOT NULL,
+    CName       CHAR(4)     NOT NULL,
+    CNumber     INT             NOT NULL,
+    PRIMARY KEY(SEmail, CName, CNumber),
     FOREIGN KEY(SEmail) REFERENCES Student(Email)
-);
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+      ON DELETE CASCADE       ON UPDATE CASCADE
+    );
+
 
 CREATE TABLE Exam_Quiz
 (
@@ -208,40 +104,80 @@ CREATE TABLE Exam_Quiz
     Date_           DATE            NOT NULL,
     Length_         INT             NOT NULL,
     PRIMARY KEY(Name_, Course_Name, Course_Number),
-    FOREIGN KEY(Course_Name) REFERENCES Course(CName),
-    FOREIGN KEY(Course_Number) REFERENCES Course(CNumber)
-    ON DELETE CASCADE       ON UPDATE CASCADE             
+    FOREIGN KEY(Course_Name, Course_Number) REFERENCES Course(CName, CNumber)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
-CREATE TABLE Student_Exam
+CREATE TABLE Exam_Topic
+    (ExamName   VARCHAR(15)     NOT NULL,
+    Topic       VARCHAR(15)     NOT NULL,
+    PRIMARY KEY(ExamName, Topic),
+    FOREIGN KEY(ExamName) REFERENCES Exam_Quiz(Name_)
+      ON DELETE CASCADE       ON UPDATE CASCADE
+    );
+
+CREATE TABLE To_Do_List
 (
+    ListID          INT             NOT NULL,
     SEmail          VARCHAR(32)     NOT NULL,
-    Course_Name     CHAR(4)         NOT NULL,
-    Course_Number   INT             NOT NULL,
-    EQName           VARCHAR(32)     NOT NULL,
-
-    --not sure about having a primary key made entirely out of foreign keys
-    --would sql be able to run it, maybe move tables with dependencies to the bottom
-    PRIMARY KEY(SEmail, Course_Name, Course_Number, EQName),
-    FOREIGN KEY(SEmail) REFERENCES Student(Email),
-    FOREIGN KEY(Course_Name) REFERENCES Course(CName),
-    FOREIGN KEY(Course_Number) REFERENCES Course(CNumber),
-    FOREIGN KEY(EQName) REFERENCES Exam_Quiz(Name_),
-    ON DELETE CASCADE       ON UPDATE CASCADE
+    PRIMARY KEY(ListID),
+    FOREIGN KEY(SEmail) REFERENCES Student(Email)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
+
+CREATE TABLE Tasks
+(
+    ListID          INT             NOT NULL,
+    Task            VARCHAR(50)     NOT NULL,
+    PRIMARY KEY(ListID, Task),
+    FOREIGN KEY(ListID) REFERENCES To_Do_List(ListID)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Assignment
+    (Name_      VARCHAR(15)     NOT NULL,
+    CNumber     INT 	        NOT NULL,
+    CName       CHAR(4)     	NOT NULL,
+    Weight_     INT             NOT NULL,
+    Due_Date    DATE            NOT NULL,
+    Descrip     VARCHAR(15),
+    Contact     VARCHAR(15),
+    ListID      INT             NOT NULL,
+    PRIMARY KEY(Name_, CNumber, CName),
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(ListID) REFERENCES To_Do_List(ListID)
+    );
+
+
+
+CREATE TABLE Completes_Assignmetns
+    (SEmail     VARCHAR(15)     NOT NULL,
+    AName       VARCHAR(15)     NOT NULL,
+    CName       CHAR(4)     	NOT NULL,
+    CNumber     INT 	        NOT NULL,
+    PRIMARY KEY(SEmail, AName, CName, CNumber),
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(AName) REFERENCES Assignment(Name_)
+      ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(SEmail) REFERENCES Student(Email)
+      ON DELETE CASCADE       ON UPDATE CASCADE
+    );
+
+
 
 CREATE TABLE Class_Meeting
 (
     MeetingName     VARCHAR(32)     NOT NULL,
-    Room#           VARCHAR(32)     NOT NULL,
-    -- maybe allow null for room# and replace null with "online"
+    RoomNum         VARCHAR(32)     DEFAULT NULL,
     Topic           VARCHAR(32),
     Course_Name     CHAR(4)         NOT NULL,
     Course_Number   INT             NOT NULL,
     PRIMARY KEY(MeetingName),
-    FOREIGN KEY(Course_Name) REFERENCES Course(CName),
-    FOREIGN KEY(Course_Number) REFERENCES Course(CNumber)
-    ON DELETE CASCADE       ON UPDATE CASCADE
+    FOREIGN KEY(Course_Name, Course_Number) REFERENCES Course(CName, CNumber)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
 CREATE TABLE Lecture
@@ -252,6 +188,7 @@ CREATE TABLE Lecture
     
     PRIMARY KEY(MeetingName_Lec),
     FOREIGN KEY(MeetingName_Lec) REFERENCES Class_Meeting(MeetingName)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
 CREATE TABLE Lab
@@ -294,6 +231,7 @@ CREATE TABLE Speaker
     -- are all of these supposed to be a key, review ER
     PRIMARY KEY(MeetingName_Sp, Name_, Organization, Credentials),
     FOREIGN KEY(MeetingName_Sp) REFERENCES Seminar(MeetingName_Sem)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
 CREATE TABLE Scheduled_Time_Slot
@@ -303,9 +241,91 @@ CREATE TABLE Scheduled_Time_Slot
     -- not sure about not null/ primary key on Dayofweek
     TimeOfDay        VARCHAR(32)     NOT NULL,
     -- format so refers to time and duration?
-    Frequency        VARCHAR(16)     NOT NULL
+    Frequency        VARCHAR(16)     NOT NULL,
     
     PRIMARY KEY(MeetingName_, DaysOFWeek, TimeOfDay, Frequency),
     FOREIGN KEY(MeetingName_) REFERENCES Class_Meeting(MeetingName)
+     ON DELETE CASCADE       ON UPDATE CASCADE
 );
 
+
+CREATE TABLE Attends_Class
+(
+    CMeetingName    VARCHAR(25)     NOT NULL,
+    SEmail          VARCHAR(32)     NOT NULL,
+    PRIMARY KEY(CMeetingName, SEmail),
+    FOREIGN KEY(CMeetingName) REFERENCES Class_Meeting(MeetingName)
+     ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(SEmail) REFERENCES Student(Email)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Section 
+(
+    CName           CHAR(4)         NOT NULL,
+    CNumber         INT             NOT NULL,
+    LectureSection  CHAR(3)         NOT NULL,
+    Semester        VARCHAR(10)     NOT NULL,
+    PRIMARY KEY(CName, CNumber, LectureSection, Semester),
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Group_Meeting
+(
+    ID              INT             NOT NULL,
+    Name_           VARCHAR(15)     NOT NULL,
+    CName           CHAR(4)         NOT NULL,
+    CNumber         INT             NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY(CName, CNumber) REFERENCES Course(CName, CNumber)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Group_Meeting_Date
+(
+    GroupID         INT             NOT NULL,
+    Date_           DATE            NOT NULL,
+    PRIMARY KEY(GroupID, Date_),
+    FOREIGN KEY(GroupID) REFERENCES Group_Meeting(ID)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+CREATE TABLE Group_Meeting_Members
+(
+    GroupID         INT             NOT NULL,
+    Member          VARCHAR(50)     NOT NULL,
+    PRIMARY KEY(GroupID, Member),
+    FOREIGN KEY(GroupID) REFERENCES Group_Meeting(ID)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+CREATE TABLE Attends_Group_Meeting
+(
+    MeetingID       INT             NOT NULL,
+    SEmail          VARCHAR(32)     NOT NULL,
+    PRIMARY KEY(MeetingID, SEmail),
+    FOREIGN KEY(MeetingID) REFERENCES Group_Meeting(ID)
+     ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(SEmail) REFERENCES Student(Email)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Student_Exam
+(
+    SEmail          VARCHAR(32)     NOT NULL,
+    Course_Name     CHAR(4)         NOT NULL,
+    Course_Number   INT             NOT NULL,
+    EQName           VARCHAR(32)     NOT NULL,
+    PRIMARY KEY(SEmail, Course_Name, Course_Number, EQName),
+    FOREIGN KEY(SEmail) REFERENCES Student(Email)
+     ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(Course_Name, Course_Number) REFERENCES Course(CName, CNumber)
+     ON DELETE CASCADE       ON UPDATE CASCADE,
+    FOREIGN KEY(EQName) REFERENCES Exam_Quiz(Name_)
+     ON DELETE CASCADE       ON UPDATE CASCADE
+);
