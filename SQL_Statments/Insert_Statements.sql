8b73786993d2eef15d7b0103130484f83049bd83
NathanRomphf <nathan.romphf@ucalgary.ca>
2022-11-12 20:46:16 -0700

    Fixed Insert statements format

--- SQL_Statments/Insert_Statements.sql
+++ SQL_Statments/Insert_Statements.sql
@@ -1,69 +1,57 @@
 INSERT INTO Admin_ ('example@gmail.com', 'CPSC471', 'Main Admin', 'Admin');
---why does admin have role?
--- guessing it implies the head admin cannot be deleted - M.Alshoura
 
-INSERT INTO Admin_ ();
---need user input
+INSERT INTO Admin_ (@Email, @Password_, @Name_, @Role_);
 
-INSERT INTO User_();
---need specific input
+INSERT INTO User_(@Email, @FName, @LName, @Password_, @UserType, @AdminEmail);
 
-INSERT INTO Student(Email, Major, Year_)
+INSERT INTO Student(@Email, @Major, @Year_);
 
-INSERT INTO Schedule_(ID, StartDate, EndDate, Year_, SemName, StudentEmail)
+INSERT INTO Schedule_(@ID, @StartDate, @EndDate, @Year_, @SemName, @StudentEmail);
 
-INSERT INTO Viewer(Email, SEmail, SchedID)
+INSERT INTO Viewer(@Email, @SEmail, @SchedID);
 
-INSERT INTO Course(CName, Descrip) VALUES ('". $cname. "','". $description ."');
---where $cname and $description are user input
+INSERT INTO Course(@CName, @CNumber, @Descrip);
 
-INSERT INTO Student_Takes(StuEmail, CName, CNUmber, Grade)
+INSERT INTO Student_Takes(@StuEmail, @CName, @CNUmber, @Grade);
 
-INSERT INTO Student_Course(SEmail, CName, CNUmber)
+INSERT INTO Student_Course(@SEmail, @CName, @CNumber);
 
-SELECT(Course_Name, Course_Number FROM Course WHERE Cname = userInput AND CNumber = userInput) INTO @some_input_var
-INSERT INTO Exam_Quiz (Name_, Course_Name, Course_Number, Weight_, Chapters, Hall, Date_, Length_)
-VALUES ('". $Name_."', '". Course_Name."', '". Course_Number."', '". Weight_."', '". Chapters."', '". Hall."', '". Date_."', '".Length_ ."')
+INSERT INTO Exam_Quiz (@Name_, @Course_Name, @Course_Number, @Weight_, @Chapters, @Hall, @Date_, @Length_);
 
-INSERT INTO Exam_Topic(ExamName, Topic)
+INSERT INTO Exam_Topic(@ExamName, @Topic);
 
-INSERT INTO To_Do_List(ListID, SEmail)
+INSERT INTO To_Do_List(@ListID, @SEmail);
 
-SELECT ListID from To_Do_List WHERE some_input_var.StuEmail = StuEmail));
-INSERT INTO Task(ListID, Task)
+INSERT INTO Task(@ListID, @Task);
 
-INSERT INTO Assignment(Name_, CNumber, CName, Weight_, Due_Date, Descrip, Contact, ListID)
+INSERT INTO Assignment(@Name_, @CNumber, @CName, @Weight_, @Due_Date, @Descrip, @Contact, @ListID);
 
-INSERT INTO Completes_Assignments(SEmail, AName, CName. CNumber)
+INSERT INTO Completes_Assignments(@SEmail, @AName, @CName. @CNumber);
 
-SELECT (CName, CNumber FROM COURSE WHERE CName = userInput AND CNumber = userInput) INTO @some_input_var
-INSERT INTO Class_Meeting (MeetingName, Room#, Topic, Course_Name, Course_Number) 
-VALUES ('". $MeetingName."','". $Room#."','". $Topic. "', '". $Cname."','". CNum."');
+INSERT INTO Class_Meeting (@MeetingName, @RoomNum, @Topic, @Course_Name, @Course_Number);
 
-INSERT INTO Lecture(MeetingName_Lec, Learning_Obj, Ch_dicussed)
+INSERT INTO Lecture(@MeetingName_Lec, @Learning_Obj, @Ch_dicussed);
 
-INSERT INTO Lab(MeetingName_Lab, Lab_topic, Due_Date, TA_Name)
+INSERT INTO Lab(@MeetingName_Lab, @Lab_topic, @Due_Date, @TA_Name);
 
-INSERT INTO Tutorial(MeetingName_Tut, TA_Name)
+INSERT INTO Tutorial(@MeetingName_Tut, @TA_Name);
 
-INSERT INTO Seminar(MeetingName_Sem)
+INSERT INTO Seminar(@MeetingName_Sem);
 
-INSERT INTO Speaker(MeetingName_Sp, Name_, Organization, Credentials)
+INSERT INTO Speaker(@MeetingName_Sp, @Name_, @Organization, @Credentials);
 
-INSERT INTO Scheduled_Time_Slot(MeetingName_, DaysOFWeek, TimeOfDay, Frequency)
+INSERT INTO Scheduled_Time_Slot(@MeetingName_, @DaysOFWeek, @TimeOfDay, @Frequency);
 
-INSERT INTO Attends_Class(CMeetingName, SEmail)
+INSERT INTO Attends_Class(@CMeetingName, @SEmail);
 
-INSERT INTO Section(CName, CNumber, LectureSection, Semester)
+INSERT INTO Section(@CName, @CNumber, @LectureSection, @Semester);
 
-SELECT (CName, CNumber from Course Where Cname = userInputName AND CNumber = userInputNum) INTO @some_input_var;
-INSERT Into Group_Meeting (Name_, CName, CNumber)
-Values (temp_name, some_input_var.CName, some_input_var.CNumber);
+INSERT Into Group_Meeting (@Name_, @CName, @CNumber);
 
-INSERT INTO Group_Meeting_Date(GroupID, Date_)
+INSERT INTO Group_Meeting_Date(@GroupID, @Date_);
 
-INSERT INTO Group_Meeting_Members(GroupID, Member)
+INSERT INTO Group_Meeting_Members(@GroupID, @Member);
 
-INSERT INTO Attends_Group_Meeting(GroupID, Member)
+INSERT INTO Attends_Group_Meeting(@GroupID, @Member);
 
-INSERT INTO Student_Exam(SEmail, Course_Name, Course_Number, EQName)
+INSERT INTO Student_Exam(@SEmail, @Course_Name, @Course_Number, @EQName)
