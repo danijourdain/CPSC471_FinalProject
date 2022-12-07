4a7d9124414b1fecf8752e6980c44e7cb1a6f15a
Mohammed Alshoura <mohammed.alshoura@ucalgary.ca>
2022-12-06 19:57:43 -0700

    Error checking add schedule

--- Website/add-schedule-viewer.php
+++ Website/add-schedule-viewer.php
@@ -8,11 +8,20 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    
-    $insert_viewer = $con->prepare("INSERT INTO Viewer (Email, SEmail, SchedID) VALUES (?, ?, ?)");
-    $insert_viewer->bind_param("ssi", $_SESSION['user-email'], $_POST['Femail'], $_POST['ID']);
-    $insert_viewer->execute();
-    header("Location: view-schedules.php");
-                    die();
+    $select_viewer = $con->prepare("SELECT * FROM Viewer WHERE Email = ? AND SEmail = ? AND SchedID = ?");
+    $select_viewer->bind_param("ssi", $_SESSION['user-email'], $_POST['Femail'], $_POST['ID']);
+    $select_viewer->execute();
+    $select_viewer = $select_viewer->get_result();
+
+    if ($select_viewer->num_rows != 0) {
+        $insert_viewer = $con->prepare("INSERT INTO Viewer (Email, SEmail, SchedID) VALUES (?, ?, ?)");
+        $insert_viewer->bind_param("ssi", $_SESSION['user-email'], $_POST['Femail'], $_POST['ID']);
+        $insert_viewer->execute();
+        header("Location: view-schedules.php");
+        die();
+    }else{
+        echo '<script>alert("Please enter a valid email and id")</script>';
+        header("Location: view-schedules.php");
+    }
                     //move to the weekly schedule page
 ?>
