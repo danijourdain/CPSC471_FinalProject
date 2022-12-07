9e148b953319bced57c5fda40a2d0e671abafb25
danijourdain <danijourdain@icloud.com>
2022-12-07 10:56:39 -0700

    Revert "Merge branch 'sqlStuff'"
    This reverts commit 708cb43612a9e02620e03ac2d6ca7b064eddf332, reversing
    changes made to a9e18ed6ae6e7964ffb0b487604317dd041e2237.

---
+++ SQL_Statements_Actual/Insert_Statements.sql
@@ -0,0 +1,5 @@
+INSERT INTO User_ (Email, FName, LName, Password_, UserType) VALUES (?, ?, ?, ?, ?);
+
+INSERT INTO Student (Email, Major, Year_) VALUES (?, ?, ?);
+
+INSERT INTO Viewer (Email, SEmail, SchedID) VALUES (?, ?, ?);
